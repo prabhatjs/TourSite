b45f782b3f966dbfd00d60d5ef3cf2a2e4b97a15
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-01-29 22:17:16 +0530

    Add get post getTourby id

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const app=express();
+const fs=require('fs');
+const port=3000;
+app.use(express.json());//change all incoming data into json middelware,
+    //!request from clinet side---response from server side,status method use for api status code 
+    const data=JSON.parse(fs.readFileSync('./data/tourdata.json','utf-8'));//parse the data in json formate 
+    app.get('/api/v1/getTour',(req,res)=>{
+        res.
+            status(200)
+            .json({
+                result:data.length,//length of data
+                message:"Hello From server",
+                app:"tourApp",
+                data:data
+            });
+        })
+        app.post('/api/v1/getTour',(req,res)=>{
+            console.log(req.body);
+            res.send('done');
+        })//if u want parameter want to option add ' ? ' this is not getting error
+        app.get('/api/v1/getTour/:id/:id2?',(req,res)=>{
+            const id=Number(req.params.id);//change the id into number
+            //handling eror of invalid ids---
+            if(id>data.length){
+                res.status(404).json({
+                    message:"fail",
+                    error:"Invalid ID"
+                });
+            }
+            //find in to json collection data ,find method take cll back function
+            const tour=data.find((tourid)=>
+                    tourid.id===id)
+            res.
+            status(200)
+            .json({
+                message:"Hello From server",
+                app:"tourApp",
+                data:tour
+            });
+        })
+
+app.listen(port,()=>{
+    console.log(`server start on ${port}`);
+})
