20e0a0c2f17c39f42cc880a7283985a9888ee95b
Prabhat Singh Yadav <prabhatmca18.jim@gmail.com>
2024-01-31 21:52:55 +0530

    Add tour model and save() test data in db

--- server/server.js
+++ server/server.js
@@ -1,11 +1,48 @@
+const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const app=require('../index.js');
 console.log(app.get("env"));//print the which enviorment you use development or production,bydefault development enviorment set
 dotenv.config({path:'config.env'});
 
 //console.log(process.env);
-const port=process.env.PORT||3000;
 
+
+
+
+mongoose.connect(process.env.DATABASE_LOCAL,{
+}).then(()=>
+    console.log("connect to Database")
+)
+
+const tourSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    price:{
+        type:Number,
+        required:true
+    }
+})
+
+const tour=mongoose.model('Tour',tourSchema);
+//saave data
+const savetour=new tour({
+    name:"Delhi Tour",
+    rating:3,
+    price:25000
+})
+savetour.save().then(doc=>{
+    console.log(doc);
+}).catch(err=>{
+    console.log(err);
+});
+
+const port=process.env.PORT||3000;
 app.listen(port,()=>{
     console.log(`server start on ${port}`);
 })
